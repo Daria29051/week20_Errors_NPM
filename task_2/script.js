4be9c99c6ee5aa6fae5b2545a3b49073a39894ff
unknown <daria.lantyugova@mail.ru>
2023-06-09 15:20:06 +0300

    charts 4 and 5 added

--- task_2/script.js
+++ task_2/script.js
@@ -1,4 +1,4 @@
-// график 1
+// график 1  (chart.js)
 const chart1 = document.querySelector('.container1__chart');
 
     new Chart(chart1,{
@@ -16,7 +16,7 @@ const chart1 = document.querySelector('.container1__chart');
     options: {} 
 });
 
-// график2
+// график2 (chart.js)
 const chart2= document.querySelector('.container2__chart');
 
     new Chart(chart2,{
@@ -51,7 +51,7 @@ const chart2= document.querySelector('.container2__chart');
     options: {}
 });
 
-// график 3
+// график 3 (chart.js)
 const chart3 = document.querySelector('.container3__chart');
 
     new Chart(chart3,{
@@ -68,3 +68,69 @@ const chart3 = document.querySelector('.container3__chart');
     } ,
     options: {}
 });
+
+
+// график4 (chartist)
+
+const chart4 = document.querySelector('.container4__chart');
+let data = {
+    labels: ['2018', '2019', '2020', '2021', '2022'],
+    series: [
+      [40, 55, 45, 40, 55],
+      [60, 45, 55, 60, 45],
+
+    ]
+  };
+
+  let options = {
+    width: 600,
+    height: 250,
+    seriesBarDistance: 40,
+    
+  };
+  
+  new Chartist.Bar(chart4, data, options);
+
+
+
+//   график5 (zingchart)
+let myConfig = {
+    type: 'line',
+    legend: {
+      draggable: true,
+    },
+    scaleX: {
+      label: { text: 'Year' },
+      labels: [ '2018', '2019', '2020', '2021', '2022' ]
+    },
+    scaleY: {
+      values: "400:1600:200" ,
+      label: { text: 'Salary, $' },
+    },
+   
+    series: [
+      {
+        // plot 1 values, linear data
+        values: [1200, 1320, 1380, 1280, 1500],
+        text: 'Science',
+      },
+      {
+        // plot 2 values, linear data
+        values: [980, 1020, 1150, 1280, 1420],
+        text: 'Languages'
+      },
+      {
+        // plot 2 values, linear data
+        values: [1100, 1120, 980, 1253, 1459],
+        text: 'Art'
+      }
+    ]
+  };
+  
+
+  zingchart.render({
+    width:600,
+    height:300,
+    id: 'myChart',
+    data: myConfig,
+  });
